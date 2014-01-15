009c235c6052d4bcb2373adf1c62116210a1aba0
Bip Thelin <bip@kivra.com>
2014-01-15 13:18:08 +0100

    Mask our gen_server calls if disabled

--- src/estatsd.erl
+++ src/estatsd.erl
@@ -10,27 +10,33 @@
 
 % Convenience: just give it the now() tuple when the work started
 timing(Key, StartTime = {_,_,_}) ->
-    Dur = erlang:round(timer:now_diff(os:timestamp(), StartTime)/1000),
-    timing(Key,Dur);
+    timing(Key, erlang:round(timer:now_diff(os:timestamp(), StartTime)/1000));
 
 % Log timing information, ms
-timing(Key, Duration) when is_integer(Duration) -> 
-    gen_server:cast(?SERVER, {timing, Key, Duration});
-
-timing(Key, Duration) -> 
-    gen_server:cast(?SERVER, {timing, Key, erlang:round(Duration)}).
+timing(Key, Duration) when is_integer(Duration) ->
+    do_cast(fun() -> gen_server:cast(?SERVER, {timing, Key, Duration}) end);
+timing(Key, Duration) ->
+    do_cast(fun() ->
+                gen_server:cast(?SERVER, {timing, Key, erlang:round(Duration)})
+            end).
 
 gauge(Key, Value) when is_integer(Value); is_float(Value) ->
-    gen_server:cast(?SERVER, {gauge, Key, Value}).
+    do_cast(fun() -> gen_server:cast(?SERVER, {gauge, Key, Value}) end).
 
 % Increments one or more stats counters
-increment(Key) -> increment(Key, 1, 1).
-increment(Key, Amount) -> increment(Key, Amount, 1).
+increment(Key)                 -> increment(Key, 1, 1).
+increment(Key, Amount)         -> increment(Key, Amount, 1).
 increment(Key, Amount, Sample) ->
-    gen_server:cast(?SERVER, {increment, Key, Amount, Sample}).
+    do_cast(fun() ->
+                gen_server:cast(?SERVER, {increment, Key, Amount, Sample})
+            end).
 
-decrement(Key) -> decrement(Key, -1, 1).
-decrement(Key, Amount) -> decrement(Key, Amount, 1).
-decrement(Key, Amount, Sample) ->
-    increment(Key, 0 - Amount, Sample).
+decrement(Key)                 -> decrement(Key, -1, 1).
+decrement(Key, Amount)         -> decrement(Key, Amount, 1).
+decrement(Key, Amount, Sample) -> increment(Key, 0 - Amount, Sample).
 
+do_cast(F) ->
+    case estatsd_sup:appvar(enabled, true) of
+        true  -> F();
+        false -> ok
+    end.
--- src/estatsd_sup.erl
+++ src/estatsd_sup.erl
@@ -4,6 +4,7 @@
 
 %% API
 -export([start_link/0, start_link/1, start_link/3]).
+-export([appvar/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
